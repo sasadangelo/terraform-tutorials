50b32e1626049f6d0098420c6e81eadf215a91df
Salvatore D'Angelo <sasadangelo@gmail.com>
2023-10-13 11:46:25 +0200

    Fix

--- lesson-06/main.tf
+++ lesson-06/main.tf
@@ -7,8 +7,25 @@ terraform {
   }
 }
 
-variable "ibmcloud_api_key" {}
+variable "ibmcloud_api_key" {
+  type        = string
+  sensitive   = true
+  description = "IBM Cloud API Key"
+  validation {
+    condition     = var.ibmcloud_api_key != ""
+    error_message = "API key for IBM Cloud must be set."
+  }
+}
 variable "region" {}
+
+data ibm_resource_group resource_group {
+  name = "${var.region}-rg"
+}
+
+data "ibm_is_vpc" "vpc" {
+    name = "${var.region}-rg-vpc"
+    depends_on = [module.vpc]
+}
 
 module "vpc" {
   source                        = "./resources/network/vpc"
@@ -17,11 +34,11 @@ module "vpc" {
 }
 
 // This module creates a public gateway if needed
-module "public_gw" {
-  count = 3
-  source          = "./resources/network/public_gw"
-  public_gw_name  = "${var.region}-gateway-${count.index + 1}"
-  vpc             = data.ibm_is_vpc.vpc.id
-  zone            = "${var.region}-${count.index + 1}"
-  resource_group  = data.ibm_resource_group.resource_group.id
-}
+#module "public_gw" {
+#  count = 3
+#  source          = "./resources/network/public_gw"
+#  public_gw_name  = "${var.region}-gateway-${count.index + 1}"
+#  vpc             = data.ibm_is_vpc.vpc.id
+#  zone            = "${var.region}-${count.index + 1}"
+#  resource_group  = data.ibm_resource_group.resource_group.id
+#}
